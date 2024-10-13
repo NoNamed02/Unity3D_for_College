28ec082de33a8602ab2ea666b36145e147b04ee1
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2024-10-13 16:47:56 +0900

    fix : 유니티 데이터 손상 복원

--- Answer_1/Assets/PlayerMove_NoCharaterController.cs
+++ Answer_1/Assets/PlayerMove_NoCharaterController.cs
@@ -1,26 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMove_NoCharaterController : MonoBehaviour
 {
-    private float _horizontal; // 좌우
-    private float _vertical; // 위아래
-    [SerializeField]
-    private float _mouseX; // 마우스 좌표값
-    private Animator animator;
-    private Vector3 cameraVelocity = Vector3.zero;
-
-    public Transform cameraTransform;
     public int speed = 10;
     public int view_speed = 100;
     public float cameraSpeed = 0.3f;
+    public Transform cameraTransform;
 
+
+    private float _horizontal; // 좌우
+    private float _vertical; // 위아래
+    private float _mouseX; // 마우스 좌표값
+    private bool _mouse0; // 마우스 좌클릭 감지
+    private Animator _animator;
+
+    private Vector3 cameraVelocity = Vector3.zero;
+    private GameObject _firePos;
+
+    private GameObject _bullet;
+    private MeshRenderer _muzzleFlash;
 
     void Start()
     {
-        _mouseX = 0f;
-        animator = GetComponent<Animator>();
+        Init();
     }
 
     void Update()
@@ -29,6 +34,20 @@ public class PlayerMove_NoCharaterController : MonoBehaviour
         Move_Object();
         Set_Animation();
         Move_Camera();
+        FireBullet();
+    }
+
+
+    private void Init()
+    {
+        _mouseX = 0f;
+        _animator = GetComponent<Animator>();
+        _firePos = GameObject.Find("FirePos");
+
+        _bullet = Resources.Load<GameObject>("Bullet");
+        
+        _muzzleFlash = GameObject.Find("Muzzle").GetComponent<MeshRenderer>();
+        _muzzleFlash.enabled = false;
     }
 
     private void Get_Input()
@@ -37,6 +56,8 @@ public class PlayerMove_NoCharaterController : MonoBehaviour
         _vertical = Input.GetAxis("Vertical");
 
         _mouseX = Input.GetAxis("Mouse X") * view_speed * Time.deltaTime;
+
+        _mouse0 = Input.GetMouseButtonDown(0);
     }
 
     private void Move_Object()
@@ -58,15 +79,60 @@ public class PlayerMove_NoCharaterController : MonoBehaviour
     private void Set_Animation()
     {
         if (new Vector3(_horizontal, 0, _vertical).magnitude > 0.5f)
-            animator.SetFloat("Speed", 1f);
+            _animator.SetFloat("Speed", 1f);
         else
-            animator.SetFloat("Speed", 0f);
+            _animator.SetFloat("Speed", 0f);
+
+        _animator.SetFloat("X", _horizontal);
+        _animator.SetFloat("Y", _vertical);
+    }
+
+    private void FireBullet()
+    {
+        if(_mouse0)
+        {
+            Instantiate(_bullet, _firePos.transform.position, Quaternion.identity); // ver 1
+            //Instantiate(_bullet, _firePos.transform.position, _muzzleFlash.gameObject.transform.rotation);
+            StartCoroutine(ShowMuzzleFlash());
+            //ShowMuzzleFlash_2rd();
+        }
     }
 
     private void Move_Camera()
     {
-        Vector3 target = transform.position + new Vector3(0f, 3f, -8f);
-        cameraTransform.position =
-        Vector3.SmoothDamp(cameraTransform.position, target, ref cameraVelocity, cameraSpeed);
+        Vector3 offset = new Vector3(0f, 3f, -8f);
+        
+        Quaternion cameraRotation = Quaternion.Euler(0f, gameObject.transform.eulerAngles.y, 0f); // 플레이어의 Y축 회전 반영
+
+        Vector3 desiredPosition = gameObject.transform.position + cameraRotation * offset;
+
+        Camera.main.transform.position = Vector3.SmoothDamp(Camera.main.transform.position, desiredPosition, ref cameraVelocity, cameraSpeed);
+
+        cameraTransform.LookAt(gameObject.transform.position + new Vector3(0f, 1.5f, 0f));
+    }
+
+    // 코루틴을 많이 사용하면 유니티가 죽는다
+    IEnumerator ShowMuzzleFlash()
+    {
+        _muzzleFlash.enabled = true;
+        _muzzleFlash.gameObject.transform.localScale = Vector3.one * Random.Range(0.3f, 0.7f);
+        _muzzleFlash.gameObject.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 90));
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.3f));
+        _muzzleFlash.enabled = false;
+    }
+
+    private void ShowMuzzleFlash_2rd() // 코루틴 일반 함수로 구현
+    {
+        _muzzleFlash.enabled = true;
+        _muzzleFlash.gameObject.transform.localScale = Vector3.one * Random.Range(0.3f, 0.7f);
+        _muzzleFlash.gameObject.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 90));
+
+        float waitTime = Random.Range(0.1f, 0.3f);
+        Invoke(nameof(DisableMuzzleFlash), waitTime);
+    }
+
+    private void DisableMuzzleFlash()
+    {
+        _muzzleFlash.enabled = false;
     }
 }
